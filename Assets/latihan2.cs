1578a94e0f3c7eed2b00bc0d006dc73e186c7470
schumyd <schumy.daraqutni21@gmail.com>
2020-09-22 21:04:35 +0700

    basicscripting

---
+++ Assets/latihan2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class latihan2 : MonoBehaviour {
+
+	public int nilaiA;
+	public int nilaiB;
+	 [Range(1, 5)]
+	 public int nilaiC;
+	void Start () 
+	{
+		int total = (nilaiA + nilaiB) * nilaiC;
+		Debug.Log("Total " + total);		
+	}
+	
+	void Update () {
+		
+	}
+}
